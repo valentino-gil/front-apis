2a167bc14d305b4aeafba4823f6748a607b5e219
Luis Ortiz <luiortiz@uade.edu.ar>
2024-10-13 21:58:26 -0300

    Redirrecionamiento de login/register hacia el home
    -boton home e inicio ya hacen scroll hacia al comienzo de pagina
    - al logearnos o registrarnos volvemos al home directamente

--- marketplace/src/components/NavBar.jsx
+++ marketplace/src/components/NavBar.jsx
@@ -3,15 +3,24 @@ import logo from '../assets/car.svg';  // logo de la pagina
 import user from '../assets/user.svg'; // icono usuario
 import heart from '../assets/heart.svg'; // icono wishlist
 import carrito from '../assets/carrito.svg'; // icono carrito
+import { Link, useNavigate } from 'react-router-dom'; // Importar Link y useNavigate
 
 const NavBar = () => {
+  const navigate = useNavigate();
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' }); // Hace scroll suave hacia el tope de la página
+    navigate("/");  // También redirige a la ruta home si es necesario
+  };
+
   return (
     <nav>
       <div className="nav-left">
-        <img src={logo} alt="Logo" className="logo" />
+        {/* Enlace al home y scroll al tope */}
+        <img src={logo} alt="Logo" className="logo" onClick={scrollToTop} style={{ cursor: 'pointer' }} />
       </div>
       <ul className="nav-center">
-        <li>Inicio</li>
+        <li onClick={scrollToTop} style={{ cursor: 'pointer' }}>Inicio</li> {/* Agregado onClick */}
         <li>Vehículos</li>
         <li>Nosotros</li>
         <li>Servicios</li>
@@ -27,5 +36,3 @@ const NavBar = () => {
 };
 
 export default NavBar;
-
-  
